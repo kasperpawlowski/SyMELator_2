05e973aefdcd298814ae57c0d4fb900081d3222e
Kasper Pawlowski <kasperpawlowski@gmail.com>
2017-06-28 14:28:58 +0200

    project prototype

---
+++ SyMELator_2/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 28.06.2017 14:14:41
+ * Author : Kasper
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
